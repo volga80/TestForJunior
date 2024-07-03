5d93417817bb65ead3d38b6937cefbd128da5569
Andrey <volgin_andrey27@mail.ru>
2024-07-03 11:29:49 +0500

    Update WalletRepository.java
    add method findByIdForUpdate with SQL Query

--- src/main/java/com/volga/testForJunior/dao/WalletRepository.java
+++ src/main/java/com/volga/testForJunior/dao/WalletRepository.java
@@ -4,13 +4,15 @@ import com.volga.testForJunior.domain.Wallet;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 
-    @Override
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<Wallet> findById(UUID uuid);
+    @Query("SELECT w FROM Wallet w WHERE w.id = :walletId")
+    Optional<Wallet> findByIdForUpdate(@Param("walletId") UUID walletId);
 }
